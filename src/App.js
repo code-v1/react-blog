a6add78cea67317dfa43636588257115767563bc
Eric Tovar <tovareric@gmail.com>
2019-07-03 11:13:32 -0500

    laying out blog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
+import './App.css';
+import Main from './Main'
+import Login from '../src/Login/Login'
+import ProtectedRoute from '../src/Login/ProtectedRoute'
+
+class App extends Component {
+  state = {
+    isLoggedIn : false
+  }
+
+
+  handleLogin = () => {
+    this.setState({ isLoggedIn: true})
+  }
+
+
+  render ()
+{   
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+      <Router>
+       <Switch>
+         <Route 
+         exact path="/" 
+         component={Login} 
+         />
+         <ProtectedRoute 
+         path="/main" 
+         isLoggedIn={this.state.isLoggedIn}
+         component={Main}
+          />
+       </Switch>
+      </Router>
+
     </div>
   );
 }
+};
 
 export default App;
